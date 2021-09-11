81ffb7d0ca336694d141b26c49b619222d2aaf91
Ben Lilley <benlilley1025@gmail.com>
2021-09-11 11:57:28 -0400

    AOC 2017 - 6 Solution

---
+++ 2017/6/main.py
@@ -0,0 +1,32 @@
+#Open file and compress lines into an array
+with open("input.txt") as f:
+    content = f.readlines()
+content = [x.split() for x in content]
+content = content[0]
+for num in range(len(content)):
+    content[num] = int(content[num])
+
+def find_cycles(content):
+    past_mem = []
+    past_mem.append(content.copy())
+    cycles = 0
+    while True:
+        max_num = max(content)
+        max_index = content.index(max_num)
+        content[max_index] = 0
+        i = max_index + 1
+        while max_num > 0:
+            content[i % len(content)] += 1
+            max_num -= 1
+            i += 1
+        cycles += 1
+        if content.copy() in past_mem:
+            break
+        past_mem.append(content.copy())
+    return [cycles, content]
+
+results = find_cycles(content)
+answer_one = results[0]
+answer_two = find_cycles(results[1])[0]
+print("p1: " + str(answer_one))
+print("p2: " + str(answer_two))
