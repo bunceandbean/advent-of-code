f4be09a3176b6dc18d39a2ba209733d401ea44a0
Ben Lilley <benlilley1025@gmail.com>
2021-10-18 20:18:38 -0400

    AOC 2017 - 17 Solution
    The first part of this was pretty straight-forward. But man oh man did I get stuck in the second part. I knew there had to be a faster way than what I did in part one, so I tried finding a pattern/cycle similar to the dancing day puzzle. Turns out, there was no distinct pattern. After ripping my hair out, I decided to simply go through the buffer numerically with not array to get the value quicker.

--- 2017/day17/main.py
+++ 2017/day17/main.py
@@ -1,4 +1,25 @@
+input = 303
+def spinlock(times):
+    buffer = [0]
+    cur_index = 0
+    for i in range(1,times+1):
+        new_index = ((cur_index + input) % i) + 1
+        buffer.insert(new_index,i)
+        cur_index = new_index
+    return buffer[cur_index +1]
 
+def quicklock(times):
+    loc = 0
+    after_zero = 0
+    for i in range(1,times+1):
+        loc = (input+loc+1) % i
+        if loc == 0:
+            after_zero = i
+    return after_zero
 
+times_2017 = spinlock(2017)
+zero_50mil = quicklock(50000000)
+answer_one = str(times_2017)
+answer_two = str(zero_50mil)
 print("p1: " + answer_one)
 print("p2: " + answer_two)
