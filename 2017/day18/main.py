d4d6b7e2031b5cb2c8bb6f422db10a3dde052aea
Ben Lilley <benlilley1025@gmail.com>
2021-10-22 02:16:17 -0400

    AOC 2017 - 18 Solution
    Jeez. I attempted to mess with eval/exec for the first part and got confused quickly. However, it turned out okay and worked. But man, part two killed me. I spent so long trying to work out the logic errors in my program, only for the bugs to be dumb errors (jgz can have a int input, .isdigit() returns false for negative numbers [this one hurt the most]). But hey, it was interesting to look at assembly-like code.

---
+++ 2017/day18/main.py
@@ -0,0 +1,87 @@
+#Open file and compress content[i]s into an array
+with open("input.txt") as f:
+    content = f.read().split("\n")
+    content = content[:len(content)-1]
+
+ops = {
+"set":"=",
+"add": "+=",
+"mul":"*=",
+"mod": "%=",
+}
+def single_assembly():
+    dex = 0
+    while dex < len(content):
+        cmd = content[dex][:3]
+        var = content[dex][4:5]
+        if var not in locals():
+            exec(var + " = 0")
+        if cmd in ops:
+            val = content[dex][6:]
+            exec(var + ops[cmd] + val)
+        elif cmd == "snd":
+            exec("snd" + ops["set"] + var)
+        elif cmd == "jgz" and eval(var + "> 0"):
+            val = content[dex][6:]
+            dex += int(eval("val"))
+            continue
+        elif cmd == "rcv" and eval(var + "!= 0"):
+            break
+        dex += 1
+    return eval("snd")
+
+def duet_assembly():
+    dex = [0,0]
+    queue = [[],[]]
+    pID = 0
+    p1Vals = 0
+    vars = [{"p":0},{"p":1}]
+    upper_bound = 10000000
+    i = 0
+    while dex[0] < len(content) and dex[1] < len(content) and i < upper_bound:
+        i += 1
+        cmd = content[dex[pID]][:3]
+        var = content[dex[pID]][4:5]
+        ####Check Vals###############
+        if len(content[dex[pID]]) >= 7:
+            val = content[dex[pID]][6:]
+            if val.isdigit() or val[1:].isdigit():
+                val = int(val)
+            else:
+                val = vars[pID][val]
+        if var not in vars[pID]:
+            vars[pID][var] = 0
+        ##############################
+        if cmd == "set":
+            vars[pID][var] = val
+        elif cmd == "add":
+            vars[pID][var] += val
+        elif cmd == "mul":
+            vars[pID][var] *= val
+        elif cmd == "mod":
+            vars[pID][var] %= val
+        elif cmd == "jgz":
+            if var.isdigit() or var[1:].isdigit():
+                jgzVal = int(var)
+            else:
+                jgzVal = vars[pID][var]
+            if jgzVal > 0:
+                dex[pID] += val
+                continue
+        elif cmd == "snd":
+            queue[(pID + 1) % 2].append(vars[pID][var])
+            if pID == 1:
+                p1Vals += 1
+        elif len(queue[pID]) > 0:
+            vars[pID][var] = queue[pID].pop(0)
+        else:
+            pID = (pID + 1) % 2
+            continue
+        dex[pID] += 1
+    return p1Vals
+
+answer_one = str(single_assembly())
+answer_two = str(duet_assembly())
+
+print("p1: " + answer_one)
+print("p2: " + answer_two)
