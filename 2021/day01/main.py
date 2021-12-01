d0e8a3a380fd33a8cfde63b842cd6eb5f057f0ba
Ben Lilley <benlilley1025@gmail.com>
2021-12-01 00:22:54 -0500

    AOC 2021 - 1 Solution
    Woohoo! AOC is back! Managed to get #217 for part one, and also managed to read part 2 incorrectly multiple times...
    
    I'm ready for a great year!

--- 2021/day01/main.py
+++ 2021/day01/main.py
@@ -1,9 +1,8 @@
 with open("input.txt") as f:
-    content = f.read().split("\n")
+    nums = list(map(int,f.read().split("\n")[:~0]))
 
-
-
-#answer_one =
-#answer_two = 
-#print("p1: " + answer_one)
-#print("p2: " + answer_two)
+sums = [sum(nums[i:i+3]) for i in range(len(nums))]
+answer_one = str(len([i for i in range(1,len(nums)) if nums[i]>nums[i-1]]))
+answer_two = str(len([i for i in range(1,len(sums)) if sums[i]>sums[i-1]]))
+print("p1: " + answer_one)
+print("p2: " + answer_two)
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # advent-of-code
  Repository of all my Advent of Code solutions.
 ### Year Progress
- [[2021]](2021)  
+ [[2021]](2021) - 02 :star:  
  [[2020]](2020) - 32 :star:  
  [[2019]](2019) - 02 :star:  
  [[2018]](2018) - 04 :star:  
@@ -9,5 +9,5 @@
  [[2016]](2016) - 20 :star:  
  [[2015]](2015) - 42 :star:  
 
-#### Total Stars - 150 :star:
+#### Total Stars - 152 :star:
 
