b225c40e1015b5454f165087158326589032da57
Ben Lilley <benlilley1025@gmail.com>
2021-12-01 00:33:07 -0500

    AOC 2021 - 1 Optimization

--- 2021/day01/main.py
+++ 2021/day01/main.py
@@ -1,8 +1,6 @@
 with open("input.txt") as f:
     nums = list(map(int,f.read().split("\n")[:~0]))
-
-sums = [sum(nums[i:i+3]) for i in range(len(nums))]
-answer_one = str(len([i for i in range(1,len(nums)) if nums[i]>nums[i-1]]))
-answer_two = str(len([i for i in range(1,len(sums)) if sums[i]>sums[i-1]]))
+answer_one = str(sum(1 for i in range(1,len(nums)) if nums[i]>nums[i-1]))
+answer_two = str(sum(1 for i in range(3,len(nums)+1) if sum(nums[i-3:i])<sum(nums[i-2:i+1])))
 print("p1: " + answer_one)
 print("p2: " + answer_two)
