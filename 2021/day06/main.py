91db42e9f87751690247bcadf2a95a7619b5938a
Ben Lilley <benlilley1025@gmail.com>
2021-12-06 00:54:20 -0500

    AOC 2021 - 6 Optimization

--- 2021/day06/main.py
+++ 2021/day06/main.py
@@ -4,14 +4,12 @@ with open("input.txt") as f:
 
 def lantern(amt,content):
     for i in range(amt):
-        new = [0 for i in range(9)]
-        for i in range(0,9):
-            new[i] = content[(i+1)%9]
-        new[6] += content[0]
-        content = new.copy()
+        app = content.pop(0)
+        content[6] += app
+        content.append(app)
     return sum(content)
 
-answer_one = str(lantern(80,content))
-answer_two = str(lantern(256,content))
+answer_one = str(lantern(80,content.copy()))
+answer_two = str(lantern(256,content.copy()))
 print("p1: " + answer_one)
 print("p2: " + answer_two)
