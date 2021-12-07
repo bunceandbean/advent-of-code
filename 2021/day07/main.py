baa8b0890336b8e3045f6fb3fcd1d97598c899ee
Ben Lilley <benlilley1025@gmail.com>
2021-12-07 00:48:31 -0500

    AOC 2021 - 7 Optimization
    Used mean and median to speed this up!

--- 2021/day07/main.py
+++ 2021/day07/main.py
@@ -1,15 +1,14 @@
+from statistics import median
+from statistics import mean
 with open("input.txt") as f:
     content = [*eval(f.read().split("\n")[:~0][0])]
 
 def fuel(expense):
-    best = 100000000
-    for x in range(max(content)):
-        sums = 0
-        for m in content:
-            sums += sum({*range(abs(m-x)+1)}) if expense else abs(m-x)
-        best = sums if sums < best else best
-    return best
-    
+    med = int(median(content))
+    mid = int(mean(content))
+    return sum([sum({*range(abs(x-mid)+1)}) for x in content]) \
+    if expense else sum([abs(x-med) for x in content])
+
 answer_one = str(fuel(False))
 answer_two = str(fuel(True))
 print("p1: " + answer_one)
