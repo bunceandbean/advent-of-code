d81b21be713781bd04073378a8846003f438ca0d
Ben Lilley <benlilley1025@gmail.com>
2021-12-08 21:19:19 -0500

    AOC 2021 - 8 Optimization
    Switched from brute force to a more systematic and logical way of thinking.  I made a dictionary of values of the signals compared to the "1" signal and "4" signal along with the length of the signal. This generates a unique tuple for each number that isn't easy. Fun set algebra concepts!

--- 2021/day08/main.py
+++ 2021/day08/main.py
@@ -1,41 +1,29 @@
-from itertools import permutations
 with open("input.txt") as f:
     content = f.read().split("\n")[:~0]
 
 total = 0
-t_sum = 0
-for x in content:
-    sums = ""
-    guide = x[:x.index("|")-1].split(" ")
-    out = x[x.index("|")+2:].split(" ")
-    easy = [2,3,4,7]
+out_sum = 0
+for line in content:
+    guide = line[:line.index("|")-1].split(" ")
+    out = line[line.index("|")+2:].split(" ")
+    easy = {2:1,3:7,4:4,7:8}
+    maps = {(2,3,6):0,(1,2,5):2,(2,3,5):3,(1,3,5):5,(1,3,6):6,(2,4,6):9}
     for num in out:
         if len(num) in easy:
             total += 1
-    valid = ['abcefg','cf','acdeg','acdfg','bcdf','abdfg','abdefg','acf','abcdefg','abcdfg']
-    found = {'a':'','b':'','c':'','d':'','e':'','f':'','g':''}
-    perms = permutations([*found.keys()])
-    for perm in perms:
-        perm = iter(perm)
-        for chr in found.keys():
-            found[chr] = next(perm)
-        ok = True
-        for num in guide:
-            build = ""
-            for chr in num:
-                build += found[chr]
-            if "".join(sorted(build)) not in valid:
-                ok = False
-                break
-        if ok:
-            for num in out:
-                build = ""
-                for chr in num:
-                    build += found[chr]
-                sums += str(valid.index("".join(sorted(build))))
-            t_sum += int(sums)
+    route = ["" for x in range(10)]
+    for signal in guide:
+        if len(signal) in easy:
+            route[easy[len(signal)]] = set(signal)
+    for signal in guide:
+        sig = set(signal)
+        if sig not in route:
+            r_1 = len(sig.intersection(route[1]))
+            r_4 = len(sig.intersection(route[4]))
+            route[maps[(r_1,r_4,len(signal))]] = sig
+    out_sum += int("".join([str(route.index(set(signal))) for signal in out]))
 
 answer_one = total
-answer_two = t_sum
+answer_two = out_sum
 print("p1:",answer_one)
 print("p2:",answer_two)
