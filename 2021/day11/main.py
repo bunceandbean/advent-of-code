abcac576a9baac6ca7e8fc0123402e9b9f18e0b5
Ben Lilley <benlilley1025@gmail.com>
2021-12-11 01:35:31 -0500

    AOC 2021 - 11 Optimization

--- 2021/day11/main.py
+++ 2021/day11/main.py
@@ -1,29 +1,24 @@
 with open("input.txt") as f:
-    content = [list(map(int,list(x))) for x in f.read().split("\n")[:~0]]
+    content = [[*map(int,list(x))] for x in f.read().split("\n")[:~0]]
     next_to = [(0,-1),(0,1),(-1,0),(1,0),(1,1),(1,-1),(-1,1),(-1,-1)]
     points = set()
+
+def find_neighbors(y,x):
+    neighbors = [(y+pair[0],x+pair[1]) for pair in next_to \
+    if (y+pair[0],x+pair[1]) in points]
+    return neighbors
 
 for y in range(len(content)):
     for x in range(len(content[y])):
         points.add((y,x))
 
-def find_neighbors(line,chr):
-    neighbors = []
-    for tup in next_to:
-        if (line+tup[0],chr+tup[1]) in points:
-            neighbors.append((line+tup[0],chr+tup[1]))
-    return neighbors
-
-
 flashes = 0
 all_flash = 0
-for i in range(1000):
+for i in range(500):
     flash = set()
-    to_add = []
-    for y in range(len(content)):
-        for x in range(len(content[y])):
-            content[y][x] += 1
+    content = [[num + 1 for num in x] for x in content]
     while True:
+        to_add = []
         for y in range(len(content)):
             for x in range(len(content[y])):
                 if content[y][x] > 9:
@@ -36,12 +31,11 @@ for i in range(1000):
                 content[point[0]][point[1]] += 1
         if len(to_add) == 0:
             break
-        to_add = []
     if i < 100:
         flashes += len(flash)
     if all_flash == 0 and len(flash) == len(content[0]*len(content)):
         all_flash = i + 1
-
+        break
 
 answer_one = flashes
 answer_two = all_flash
