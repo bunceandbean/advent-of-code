f70648f8797e530eaf5c0c1627cf6a56137fc37d
Ben Lilley <benlilley1025@gmail.com>
2021-12-12 01:13:27 -0500

    AOC 2021 - 12 Solution
    Recursive paths, a classic!

---
+++ 2021/day12/main.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+with open("input.txt") as f:
+    content = f.read().split("\n")[:~0]
+
+paths = defaultdict(list)
+for line in content:
+    one,two = line.split('-')
+    paths[one].append(two)
+    paths[two].append(one)
+
+def pathfinder(pos,past,mult,cave):
+    if pos == "end":
+        return 1
+    if pos == "start" and len(past) != 0:
+        return 0
+    if pos.islower() and pos in past and not mult:
+        return 0
+    elif pos.islower() and pos in past and mult:
+        if cave == "":
+            cave = pos
+        else:
+            return 0
+    past = past | {pos}
+    poss = 0
+    for route in paths[pos]:
+        poss += pathfinder(route,past,mult,cave)
+    return poss
+
+
+answer_one = pathfinder("start",set(),False,"")
+answer_two = pathfinder("start",set(),True,"")
+print("p1:",answer_one)
+print("p2:",answer_two)
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # advent-of-code
  Repository of all my Advent of Code solutions.
 ### Year Progress
- [[2021]](2021) - 22 :star:  
- [[2020]](2020) - 32 :star:  
+ [[2021]](2021) - 24 :star:  
+ [[2020]](2020) - 34 :star:  
  [[2019]](2019) - 04 :star:  
  [[2018]](2018) - 06 :star:  
  [[2017]](2017) - 50 :star: - *Complete!*  
  [[2016]](2016) - 20 :star:  
  [[2015]](2015) - 50 :star: - *Complete!*  
 
-#### Total Stars - 184 :star:
+#### Total Stars - 188 :star:
 
