ecaf0f6cfcd0d3aeb35d7532a6f64b3c6812c209
Ben Lilley <benlilleydev@gmail.com>
2022-12-03 00:22:43 -0500

    AOC 2022 - Day 3 - Optimization

--- 2022/day03/main.py
+++ 2022/day03/main.py
@@ -7,12 +7,12 @@ run_sum_1 = 0
 run_sum_2 = 0
 for item in content:
     x,y = set(item[:len(item)//2]),set(item[len(item)//2:])
-    ch = next(iter(x.intersection(y)))
+    ch = next(iter(x & y))
     run_sum_1 += chars.index(ch) + 1
 
 for i in range(0,len(content),3):
     x,y,z = set(content[i]),set(content[i+1]),set(content[i+2])
-    ch = next(iter(x.intersection(y).intersection(z)))
+    ch = next(iter(x & y & z))
     run_sum_2 += chars.index(ch) + 1
 
 print("p1:", run_sum_1)
