b76d9166dce9ed86e8d20cdbb5aaff76935e4ff8
Ben Lilley <benlilleydev@gmail.com>
2024-12-04 20:58:44 -0500

    AOC 2019 - Day 4 X/X

--- 2019/day04/main.py
+++ 2019/day04/main.py
@@ -1,9 +1,12 @@
-with open("input.txt") as f:
-    content = f.read().split("\n")[:~0]
+input = "246540-787419"
 
+total = 0
+total_only = 0
+for i in range(int(input[:input.index("-")]), int(input[input.index("-")+1:]) + 1):
+    if not (sorted(str(i)) == list(str(i))):
+        continue
+    total += any([x in str(i) for x in ["00","11","22","33","44","55","66","77","88","99"]])
+    total_only += any([x in str(i) and x + x[0] not in str(i) for x in ["00","11","22","33","44","55","66","77","88","99"]])
 
-
-#answer_one =
-#answer_two = 
-#print("p1:", answer_one)
-#print("p2:", answer_two)
+print("p1:", total)
+print("p2:", total_only)
